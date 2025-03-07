1b1881121a04def102b615e539819a6e95439b5e
Ben Long <blong@friendsbalt.org>
2025-03-06 23:18:03 -0500

    Working on blindsystem

--- MainBot.py
+++ MainBot.py
@@ -7,12 +7,24 @@ class Bot():
         self.cash = cash
         self.seat = seat
         self.handsPlayed = hands
-        
-    def newHand(self):
-        pass
-    def bet(self):
+
+    def newHand(self, hand, blind):
+        self.hand = hand
+        self.pot = 3 * min
+        self.blind = blind
+        if blind == "B":
+            self.bet(min * 2)
+        elif blind == "b":
+            self.bet(min)
+
+    def CalcBet(self, round):
+        #isAnte?
+        return self.bet()
+    
+    def bet(self, amt):
         betAmt = self.min
         self.cash -= betAmt
         return betAmt
+    
     def stats(self):
         print("Cash: {self.cash}, rFactor: {self.rFactor}, ")
